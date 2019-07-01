b69d25db6d3375d838bffb46a69223a37a66107d
AudreyJones <audrey.eliz.jones@gmail.com>
2019-07-01 11:32:56 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,13 @@
 // Code SimpleComponent Here
+import React from 'react'
+
+class SimpleComponent extends Component{
+    // super()
+    this.state = {
+        mood: 'happy'
+    }
+    render(){
+        return <div>Hello {this.state.mood || 'happy' }</div>;
+        
+    }
+}
